073e0b7bad4fe530471703176010b8db409d1c79
Pablo Marchant <pablo.marchant@kuleuven.be>
2023-09-26 11:31:17 +0200

    fix build

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,12 @@
 using Documenter
+using Literate
+
+function ignore_code_blocks(content)
+    content = replace(content, "##\n" => "\n")  # remove code blocks
+    #content = replace(content, "###" => "##")  # make level 3 headers level 2
+end
+
+Literate.markdown("./src/1_introduction_computational.jl", "./src/", preprocess=ignore_code_blocks)
 
 makedocs(sitename="Stellar Structure and Evolution",
         pages= ["index.md",
