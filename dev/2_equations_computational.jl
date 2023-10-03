0ec97d6255b6ee9db2141c4cc5511bbbb6481c9f
Documenter.jl <documenter@juliadocs.github.io>
2023-10-03 09:03:37 +0000

    build based on 24bb66c

---
+++ dev/2_equations_computational.jl
@@ -0,0 +1,8 @@
+#=
+Let's compute some numerical solutions to the Lane-Embden equation.
+A good reference which was used in the development of this exercise is
+the book by [^Hansen2004].
+=#
+
+# [^Hansen2004]:
+#     Stellar interiors : physical principles, structure, and evolution, 2nd ed., by C.J. Hansen, S.D. Kawaler, and V. Trimble. New York: Springer-Verlag, 2004.
