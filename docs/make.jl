30dbd9e0b0a5e42bfcd4a281d96483e509ce146e
Pablo Marchant <pablo.marchant@kuleuven.be>
2023-11-07 11:45:41 +0100

    nucleo

--- docs/make.jl
+++ docs/make.jl
@@ -26,11 +26,11 @@ makedocs(sitename="Stellar Structure and Evolution",
                                               "Exercises"=>"5_radiative_problems.md"],
                 "Convection (31/10/23)" => ["Notes" => "6_convection.md",
                                               "Exercises"=>"6_convection_problems.md"],
-                "Nucleosynthesis (07/11/23)" => ["Notes" => "7_nucleo.md",
-                                              "Exercises"=>"7_nucleo_problems.md"],
-                "LAB1 (14/11/2023)" => ["Stellar evolution codes" => "8_SE_codes.md",
-                                        "Instructions" => "8_instructions.md",
-                                        "Questions"=>"8_questions.md"],
+                "Nucleosynthesis (07/11/23)" => ["Notes" => "7_nucleo1.md",
+                                              "Exercises"=>"7_nucleo1_problems.md"],
+                #"LAB1 (14/11/2023)" => ["Stellar evolution codes" => "8_SE_codes.md",
+                #                        "Instructions" => "8_instructions.md",
+                #                        "Questions"=>"8_questions.md"],
                 ])
 
 deploydocs(
